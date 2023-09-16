638a18715dd47b3997937c0d8286ffa99047ca14
Guillaume <rossetguillaume759@gmail.com>
2023-09-16 11:15:52 +0200

    gestion du header

--- rossetguillaume_8_14092023/src/components/Header/Header.js
+++ rossetguillaume_8_14092023/src/components/Header/Header.js
@@ -1,16 +1,62 @@
-import logo from "../../assets/images/logo.png";
 import "./Header.scss";
+import logo from "../../assets/images/logo.png";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Header() {
+const Header = () => {
   return (
-    <header className="Header">
-      <img src={logo} alt="Logo" className="logo" />
-      <ul>
-        <li>Accueil</li>
-        <li>A propos</li>
-      </ul>
+    <header className="header">
+      <div className="header_logo">
+        <img src={logo} alt="kasa-logo" className="header_logo_img" />
+      </div>
+      <nav className="header_nav">
+        <ul>
+          <li className="header_nav_link">
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "header_nav_link_navlink header_nav_link_active"
+                  : "header_nav_link_navlink header_nav_link_inactive"
+              }
+              title="Accueil"
+              end
+              to="/home"
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li className="header_nav_link">
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "header_nav_link_navlink header_nav_link_active"
+                  : "header_nav_link_navlink header_nav_link_inactive"
+              }
+              title="À propos"
+              end
+              to="/about"
+            >
+              A propos
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
-}
+};
 
 export default Header;
+
+// function Header() {
+//   return (
+//     <header className="Header">
+//       <img src={logo} alt="Logo" className="logo" />
+//       <ul>
+//         <li>Accueil</li>
+//         <li>A propos</li>
+//       </ul>
+//     </header>
+//   );
+// }
+
+// export default Header;
